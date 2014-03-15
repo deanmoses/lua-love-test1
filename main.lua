04945cf95813c04a3ba4de83ee4d29468959a2c8
Dean <dmoses@salesforce.com>
2014-03-15 11:16:51 -0700

    Fully encapsulated Player; main.lua now interfaces with it via player.update(), player.draw(), etc

--- main.lua
+++ main.lua
@@ -1,118 +1,44 @@
 require "pig/Pig"
 require "background/Background"
 require "player/Player"
-require "player/SpriteAnimation"
---require "camera"
 
 function love.load()
     g = love.graphics
-	
-	
-	
-	
-	width = g.getWidth()
-	height = g.getHeight()
+	width = g.getWidth() -- width of window
+	height = g.getHeight() -- height of window
 	g.setBackgroundColor(85, 85, 85)
 	
-	yFloor = 500
+	yFloor = 500 -- y position of the floor, where the player rests
 	
 	-- Set up background
 	bg = Background:new(yFloor)
 	
+    -- Set up our player
+    p = Player:new(yFloor)
+	
+	-- Set up the pig
 	pig = Pig:new()
-
-	-- Load player animation
-	animation = SpriteAnimation:new("player/robosprites.png", 32, 32, 4, 4)
-	animation:load(delay)
-		
-    playerColor = {255,0,128}
- 
- 
-    -- instantiate our player and set initial values
-    p = Player:new()
- 
-    p.x = 300
-    p.y = 300
-    p.width = 32
-	p.height = 32
-	p.jumpSpeed = -800
-	p.runSpeed = 500
-
-	gravity = 1800
-	hasJumped = false
-	delay = 120
 end
 
 function love.update(dt)
-    -- check controls
-    if love.keyboard.isDown("right") then
-        p:moveRight()
-        animation:flip(false, false)
-    end
-    if love.keyboard.isDown("left") then
-        p:moveLeft()
-        animation:flip(true, false)
-    end
-    if love.keyboard.isDown("x") and not(hasJumped) then
-        hasJumped = true
-        p:jump()
-    end
- 
-    -- update the player's position
-    p:update(dt, gravity)
- 
-    -- stop the player when they hit the borders
-    p.x = math.clamp(p.x, 0, width * 2 - p.width)
-    if p.y < 0 then p.y = 0 end
-    if p.y > yFloor - p.height then
-        p:hitFloor(yFloor)
-    end
- 
-    -- update the sprite animation
-    if (p.state == "stand") then
-        animation:switch(1, 4, 200)
-    end
-    if (p.state == "moveRight") or (p.state == "moveLeft") then
-        animation:switch(2, 4, 120)
-    end
-    if (p.state == "jump") or (p.state == "fall") then
-        animation:reset()
-        animation:switch(3, 1, 300)
-    end
-    animation:update(dt)
+    p:update(dt) -- update the player
 end
 
 function love.draw()
-	bg:draw()
-	
-	pig:draw()
-	
-    -- round down our x, y values
-    local x = math.floor(p.x)
-    local y = math.floor(p.y)
-	
-	-- draw the player
-    g.setColor(255, 255, 255)
-    animation:draw(x, y)
- 
-    -- debug information
-    g.setColor(255, 255, 255)
-    g.print("Player coordinates: ("..x..","..y..")", 5, 5)
-    g.print("Current state: "..p.state, 5, 20)
+	bg:draw() -- draw the background
+	p:draw() -- draw the player
+	pig:draw() -- draw the pig
 end
  
 function love.keyreleased(key)
     if key == "escape" then
     	love.event.quit()
     end
-    if (key == "right") or (key == "left") then
-        p:stop()
-    end
-	if (key == "x") then
-        hasJumped = false
-    end
+	
+	p:keyreleased(key)
 end
 
+-- add a useful function to the built-in math package
 function math.clamp(x, min, max)
     return x < min and min or (x > max and max or x)
 end
--- player/Player.lua
+++ player/Player.lua
@@ -1,21 +1,32 @@
+require "player/SpriteAnimation"
+
 Player = {}
  
 -- Constructor
-function Player:new()
+function Player:new(yFloor)
     -- define our parameters here
     local object = {
-	    x = 0,
-	    y = 0,
-	    width = 0,
-	    height = 0,
+	    x = 300,
+	    y = 300,
+	    width = 32,
+	    height = 32,
 	    xSpeed = 0,
 	    ySpeed = 0,
 	    state = "",
-	    jumpSpeed = 0,
-	    runSpeed = 0,
-	    canJump = false
+	    jumpSpeed = -800,
+	    runSpeed = 500,
+	    canJump = false,
+		gravity = 1800,
+		hasJumped = false,
+		delay = 120,
+		yFloor = yFloor,
+		-- Load player animation
+		animation = SpriteAnimation:new("player/robosprites.png", 32, 32, 4, 4)
     }
     setmetatable(object, { __index = Player })
+	
+	object.animation:load(object.delay)
+	
     return object
 end
 
@@ -29,11 +40,13 @@ end
 function Player:moveRight()
     self.xSpeed = self.runSpeed
     self.state = "moveRight"
+	self.animation:flip(false, false)
 end
  
 function Player:moveLeft()
     self.xSpeed = -1 * (self.runSpeed)
     self.state = "moveLeft"
+	self.animation:flip(true, false)
 end
  
 function Player:stop()
@@ -46,13 +59,25 @@ function Player:hitFloor(maxY)
     self.canJump = true
 end
 
-function Player:update(dt, gravity)
+function Player:update(dt)
+    -- check controls
+    if love.keyboard.isDown("right") then
+        self:moveRight()
+    end
+    if love.keyboard.isDown("left") then
+        self:moveLeft()
+    end
+    if love.keyboard.isDown("x") and not(self.hasJumped) then
+        self.hasJumped = true
+        self:jump()
+    end
+	
     -- update the player's position
     self.x = self.x + (self.xSpeed * dt)
     self.y = self.y + (self.ySpeed * dt)
  
     -- apply gravity
-    self.ySpeed = self.ySpeed + (gravity * dt)
+    self.ySpeed = self.ySpeed + (self.gravity * dt)
  
     -- update the player's state
     if not(self.canJump) then
@@ -70,4 +95,48 @@ function Player:update(dt, gravity)
             self.state = "stand"
         end
     end
+	
+    -- stop the player when they hit the borders
+    self.x = math.clamp(self.x, 0, width * 2 - self.width)
+    if self.y < 0 then self.y = 0 end
+    if self.y > self.yFloor - self.height then
+        self:hitFloor(self.yFloor)
+    end
+	
+    -- update the sprite animation
+    if (self.state == "stand") then
+        self.animation:switch(1, 4, 200)
+    end
+    if (self.state == "moveRight") or (self.state == "moveLeft") then
+        self.animation:switch(2, 4, 120)
+    end
+    if (self.state == "jump") or (self.state == "fall") then
+        self.animation:reset()
+        self.animation:switch(3, 1, 300)
+    end
+    self.animation:update(dt)
+end
+
+function Player:draw()
+    -- round down our x, y values
+    local x = math.floor(self.x)
+    local y = math.floor(self.y)
+	
+	-- draw the player
+    g.setColor(255, 255, 255)
+    self.animation:draw(x, y)
+ 
+    -- debug information
+    g.setColor(255, 255, 255)
+    g.print("Player coordinates: ("..x..","..y..")", 5, 5)
+    g.print("Current state: "..self.state, 5, 20)
+end
+
+function Player:keyreleased(key)
+    if (key == "right") or (key == "left") then
+        self:stop()
+    end
+	if (key == "x") then
+        self.hasJumped = false
+    end	
 end
