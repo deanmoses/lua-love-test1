3507fd797bd6422995abf19e85e1aa5db77f521f
Dean <dmoses@salesforce.com>
2014-03-15 11:20:17 -0700

    code cleanup

--- main.lua
+++ main.lua
@@ -3,30 +3,24 @@ require "background/Background"
 require "player/Player"
 
 function love.load()
-    g = love.graphics
+	g = love.graphics
 	width = g.getWidth() -- width of window
 	height = g.getHeight() -- height of window
-	g.setBackgroundColor(85, 85, 85)
 	
 	yFloor = 500 -- y position of the floor, where the player rests
-	
-	-- Set up background
-	bg = Background:new(yFloor)
-	
-    -- Set up our player
-    p = Player:new(yFloor)
-	
-	-- Set up the pig
-	pig = Pig:new()
+
+	bg = Background:new(yFloor) -- Set up background
+	player = Player:new(yFloor) -- Set up our player
+	pig = Pig:new() -- Set up the pig
 end
 
 function love.update(dt)
-    p:update(dt) -- update the player
+    player:update(dt) -- update the player
 end
 
 function love.draw()
 	bg:draw() -- draw the background
-	p:draw() -- draw the player
+	player:draw() -- draw the player
 	pig:draw() -- draw the pig
 end
  
@@ -35,7 +29,7 @@ function love.keyreleased(key)
     	love.event.quit()
     end
 	
-	p:keyreleased(key)
+	player:keyreleased(key)
 end
 
 -- add a useful function to the built-in math package
