8159c1c783f9e4bc620e4dc2a748383138e0320c
Dean <dmoses@salesforce.com>
2014-03-17 00:39:32 -0700

    Gave some love to the start screen

--- gamestates/menu/Menu.lua
+++ gamestates/menu/Menu.lua
@@ -6,13 +6,30 @@ Menu = {}
 
 function Menu:enter()
 	bg = g.newImage("gamestates/menu/bg.jpg")
+	
+	instructions = {
+		width = 240,
+		height = 220
+	}
+	
+	pressEnter = { x=200, y=0, text = "Press Enter to continue" }
+	tween(4, pressEnter, { y=300 }, 'outBounce')
+	
+	rect = { color=0 }
+	tween(4, rect, { color=255 }, 'linear')
+end
+
+function Menu:update(dt)
+	tween.update(dt)	
 end
 
 function Menu:draw()
 	g.draw(bg, 0, 0)
 	
-	g.setColor(0,0,0,200)
-	g.rectangle("fill", width/2 - 200, height/2 - 200, 250, 200)
+	
+	
+	g.setColor(0,0,0,rect.color)
+	g.rectangle("fill", 190, 100, instructions.width + 20, instructions.height)
 	
 	g.setColor(255, 255, 255)
 	local font = g.newFont("fonts/orangejuice20.ttf", 24) 
@@ -23,11 +40,11 @@ function Menu:draw()
 	"x - jump\n"..
 	"right arrow - go right\n"..
 	"left arrow: - go left\n"..
-	"space bar - shoot"..
-	"\n\n\n"..
-	"Press Enter to continue"
-	
+	"space bar - shoot"
+
 	g.print(instructions, width/2 - 200, height/2 - 200)
+	
+	g.print(pressEnter.text, pressEnter.x, pressEnter.y)
 end
 
 function Menu:keyreleased(key, code)
--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+tween = require 'libs.tween.tween'
 Gamestate = require "libs.hump.gamestate"
 require "gamestates.game.Game"
 require "gamestates.menu.Menu"
@@ -9,6 +10,10 @@ function love.load()
 	
 	Gamestate.registerEvents()
 	Gamestate.switch(Menu)
+end
+
+function love.update(dt)
+	
 end
 
 function love.keyreleased(key)
